e81ee705cd9cf599b8c2dd63e69b7f9138f9941e
Volodymyr-Karabash <voca480@gmail.com>
2025-04-11 15:07:32 +0300

    accounts updated

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -24,7 +24,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 # Login Serializer
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField()
+    # password = serializers.CharField()
 
     def validate(self, data):
         user = authenticate(**data)
