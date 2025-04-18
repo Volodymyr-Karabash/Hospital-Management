1dda78689ec1d75b60d3e0a37764bd6846496fc6
Volodymyr-Karabash <voca480@gmail.com>
2025-04-18 21:15:57 +0300

    Update Dashboard

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -4,7 +4,6 @@ from .views import ExpenseViewSet, StaffViewSet, DepartmentViewSet, PayrollViewS
 
 router = routers.DefaultRouter()
 router.register('staff', StaffViewSet, 'Staff'),
-router.register('department', DepartmentViewSet, 'Department'),
 router.register('expense', ExpenseViewSet, 'Expense'),
 router.register('payroll', PayrollViewSet, 'Payroll'),
 router.register('message', MessageViewSet, 'Message'),
